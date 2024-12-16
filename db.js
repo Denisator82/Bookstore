7a5c2ddd238686080b6863bc84adf43e36723758
Denisator82 <denis.welsch@gmx.de>
2024-12-16 14:07:25 +0100

    first commit

---
+++ db.js
@@ -0,0 +1,97 @@
+let books = [
+  {
+    "name": "Der Hobbit",
+    "author": "J.R.R. Tolkien",
+    "likes": 2500,
+    "liked": true,
+    "price": 25.99,
+    "publishedYear": 1937,
+    "genre": "Fantasy",
+    "comments": [
+      {
+        "name": "TolkienFan",
+        "comment": "Ein episches Abenteuer, das mich in eine andere Welt entführt hat."
+      },
+      {
+        "name": "MiddleEarthLover",
+        "comment": "Die Geschichte ist unglaublich und die Welt perfekt beschrieben."
+      }
+    ]
+  },
+  {
+    "name": "Die unendliche Geschichte",
+    "author": "Michael Ende",
+    "likes": 2000,
+    "liked": true,
+    "price": 18.50,
+    "publishedYear": 1979,
+    "genre": "Fantasy",
+    "comments": [
+      {
+        "name": "EndeLiebhaber",
+        "comment": "Ein Buch, das meine Kindheit geprägt hat. Absolut magisch."
+      },
+      {
+        "name": "PhantasienFan",
+        "comment": "Eine der besten Fantasiegeschichten aller Zeiten."
+      }
+    ]
+  },
+  {
+    "name": "Eragon: Das Vermächtnis der Drachenreiter",
+    "author": "Christopher Paolini",
+    "likes": 1750,
+    "liked": true,
+    "price": 21.99,
+    "publishedYear": 2002,
+    "genre": "Fantasy",
+    "comments": [
+      {
+        "name": "Drachenreiter",
+        "comment": "Ein großartiger Start einer fantastischen Serie."
+      },
+      {
+        "name": "FantasyLover",
+        "comment": "Paolinis Welt ist faszinierend und aufregend."
+      }
+    ]
+  },
+  {
+    "name": "Die Tribute von Panem - Tödliche Spiele",
+    "author": "Suzanne Collins",
+    "likes": 3000,
+    "liked": true,
+    "price": 24.99,
+    "publishedYear": 2008,
+    "genre": "Dystopie",
+    "comments": [
+      {
+        "name": "KatnissFan",
+        "comment": "Eine spannende und packende Geschichte, die mich nicht losgelassen hat."
+      },
+      {
+        "name": "DystopieLiebhaber",
+        "comment": "Ein fesselndes Buch mit starken Charakteren und einer tiefen Handlung."
+      }
+    ]
+  },
+  {
+    "name": "Winterkartoffelknödel: Ein Provinzkrimi",
+    "author": "Rita Falk",
+    "likes": 1450,
+    "liked": true,
+    "price": 19.90,
+    "publishedYear": 2010,
+    "genre": "Krimi",
+    "comments": [
+      {
+        "name": "KrimiLiebhaber",
+        "comment": "Ein humorvoller und spannender Krimi, der mich bestens unterhalten hat."
+      },
+      {
+        "name": "BayernFan",
+        "comment": "Tolle Charaktere und eine herrlich bayrische Atmosphäre."
+      }
+    ]
+  }
+];
